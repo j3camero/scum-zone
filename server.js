42d1768a253bb161733de4606653ca4dc3cb1a75
Jeff Cameron <cameron.jp@gmail.com>
2019-01-03 00:00:39 +0000

    Added a header and menu.

--- server.js
+++ server.js
@@ -8,13 +8,22 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
+function truncateLongNames(ranks) {
+    ranks.forEach((player, i) => {
+	const maxNameLength = 20;
+	if (player.name.length > maxNameLength) {
+	    ranks[i].name = player.name.substring(0, maxNameLength);
+	}
+    });
+    return ranks;
+}
+
 app.get('/', (req, res) => {
-    console.log('index');
     const json = fs.readFileSync('ranks.json');
     const ranks = JSON.parse(json);
     const t = moment.unix(ranks.timestamp);
     res.render('index', {
-	ranks: ranks.ranks,
+	ranks: truncateLongNames(ranks.ranks),
 	updateDuration: t.from(moment()),
 	updateTime: t.tz('America/Los_Angeles').format('MMM Do h:mm A'),
     });
