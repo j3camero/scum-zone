310f51b247a44289983dcfefae060d100d1860ad
Jeff Cameron <cameron.jp@gmail.com>
2019-01-04 23:56:27 +0000

    Added map to the home page.

--- public/map.js
+++ public/map.js
@@ -1,6 +1,5 @@
 
-function drawMapWithCircleZone(canvasId, zoneX, zoneY, zoneRadius) {
-    const canvas = document.getElementById(canvasId);
+function drawMapWithCircleZone(canvas, zoneX, zoneY, zoneRadius) {
     const context = canvas.getContext('2d');
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientWidth;
@@ -21,45 +20,56 @@ function drawMapWithCircleZone(canvasId, zoneX, zoneY, zoneRadius) {
     scumMap.src = '/map.jpg';
 }
 
-function drawZoomedMapWithCircleZone(canvasId, zoneX, zoneY, zoneRadius,
-				     viewSizeInMeters) {
-    const canvas = document.getElementById(canvasId);
+function drawZoomedMapWithCircleZone(canvas, zoneX, zoneY, zoneRadius,
+				     viewSizeInMeters, aspectRatio,
+				     lineWidth) {
     const context = canvas.getContext('2d');
     const w = canvas.clientWidth;
+    const h = w * aspectRatio;
     canvas.width = w;
-    canvas.height = w;
+    canvas.height = h;
     const mapSize = 12000;
     const scumMap = new Image();
     scumMap.onload = () => {
 	const v = viewSizeInMeters / 2;
+	const vh = v * aspectRatio;
 	// Transform the coords into positive quadrant.
 	zoneX = mapSize / 2 - zoneX;
 	zoneY = mapSize / 2 - zoneY;
 	const centerX = Math.min(Math.max(zoneX, v), mapSize - v);
-	const centerY = Math.min(Math.max(zoneY, v), mapSize - v);
+	const centerY = Math.min(Math.max(zoneY, vh), mapSize - vh);
 	const px = scumMap.width * (centerX - v) / mapSize;
-	const py = scumMap.height * (centerY - v) / mapSize;
+	const py = scumMap.height * (centerY - vh) / mapSize;
 	const pw = scumMap.width * viewSizeInMeters / mapSize;
-	console.log(centerX, centerY, px, py, pw);
-	context.drawImage(scumMap, px, py, pw, pw, 0, 0, w, w);
+	const ph = scumMap.height * aspectRatio * viewSizeInMeters / mapSize;
+	context.drawImage(scumMap, px, py, pw, ph, 0, 0, w, h);
 	context.beginPath();
 	context.fillStyle = 'rgba(255, 58, 133, 0.25)';
 	context.strokeStyle = 'rgb(255, 58, 133)';
-	context.lineWidth = 5;
+	context.lineWidth = lineWidth;
 	const x = (zoneX - centerX) / viewSizeInMeters * w + (w / 2);
-	const y = (zoneY - centerY) / viewSizeInMeters * w + (w / 2);
+	const y = (zoneY - centerY) / viewSizeInMeters * w + (h / 2);
 	const radius = zoneRadius / viewSizeInMeters * w;
 	context.arc(x, y, radius, 0, 2 * Math.PI);
 	context.fill();
 	context.stroke();
     };
     scumMap.src = '/map.jpg';
-    
 }
 
 function main() {
-    drawMapWithCircleZone('bigmap', -400, -4900, 500);
-    drawZoomedMapWithCircleZone('smallmap', -400, -4900, 500, 2000);
+    const bigMap = document.getElementById('bigmap');
+    if (bigMap) {
+	drawMapWithCircleZone(bigMap, -400, -4900, 500);
+    }
+    const smallMap = document.getElementById('smallmap');
+    if (smallMap) {
+	drawZoomedMapWithCircleZone(smallMap, -400, -4900, 500, 2000, 1, 5);
+    }
+    const homeMap = document.getElementById('homemap');
+    if (homeMap) {
+	drawZoomedMapWithCircleZone(homeMap, -400, -4900, 500, 9000, 0.618, 2);
+    }
 }
 
 // Self-executing function that runs after the DOM loads.
